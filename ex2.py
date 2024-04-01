7f0eb67223b30d67451c5fc49e5e87cae0bd8474
Ricardo Mahha <100898686+ricardomahha@users.noreply.github.com>
2024-04-01 14:51:08 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,15 @@
+def verifica_fibonacci(numero):
+    a, b = 0, 1
+    while b < numero:
+        a, b = b, a + b
+    if b == numero:
+        return True
+    else:
+        return False
+
+numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci: "))
+
+if verifica_fibonacci(numero):
+    print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero} não pertence à sequência de Fibonacci.")
---
+++ ex5.py
@@ -0,0 +1,9 @@
+s = "resposta exercício 5"
+l = []
+
+for letter in s:
+    l.insert(0, letter)
+
+reverse_string = "".join(l)
+
+print(reverse_string)
